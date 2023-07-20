3d9fee8f505f38bb80b854f194d54641358c2d1f
Eric Huber <ehuber@VividSolutions.com>
2023-07-20 15:25:22 -0700

    Allow Species Group and Site Species to update based on choicebox input

--- vdyp-proofofconcept/src/application/SceneController.java
+++ vdyp-proofofconcept/src/application/SceneController.java
@@ -3,13 +3,15 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.ArrayList;
+import java.util.List;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Label;
 
 
 
@@ -29,45 +31,100 @@ public class SceneController implements Initializable {
 	@FXML
 	private ChoiceBox<String> species_6;
 	
-	//Set of trees
+	// An array containing the names of different tree species
 	String[] treeSpecies = {"AC - Popular", "AT - Aspen", "B - True Fir", "BA - Amabilis Fir","BG - Grand Fir", "BL - Alpine Fir", "CW - Western Red Cedar", "DR - Red Alder",
 							"E - Birch", "EA - Alaska Paper Birch", "EP - Common Paper Birch", "FD - Douglas Fir", "H - Hemlock", "HM - Mountain Hemlock", "HW - Western Hemlock",
 							"L - Larch","LA - Alpine Larch", "LT - Tamarack", "LW - Western Larch", "MB - Bigleaf Maple","PA - Whitebark Pine", "PF - Limber Pine", "PJ - Jack Pine",
 							"PL - Lodgepole Pine", "PY - Western White Pine", "PY - Ponderosa (Yellow) Pine", "S - Spruce", "SB - Black Spruce","SE - Engelmann Spruce", "SS - Sitka Spruce",
 							"SW - White Spruce", "YC - Yellow Cedar"
 							};
+
 	
-	String currentSpecies_1;
-	
+	// Labels for display based on choice boxes
+	@FXML
+    private Label species1Group;
+    @FXML
+    private Label species1Site;
+    @FXML
+    private Label species2Group;
+    @FXML
+    private Label species2Site;
+    @FXML
+    private Label species3Group;
+    @FXML
+    private Label species3Site;
+    @FXML
+    private Label species4Group;
+    @FXML
+    private Label species4Site;
+    @FXML
+    private Label species5Group;
+    @FXML
+    private Label species5Site;
+    @FXML
+    private Label species6Group;
+    @FXML
+    private Label species6Site;
+    
+    @FXML
+    private List<ChoiceBox<String>> speciesChoiceBoxes = new ArrayList<>();
+
+    
+    // Labels for display based on choice boxes in an array
+    @FXML
+    private Label[] speciesGroups = new Label[6];
+    @FXML
+    private Label[] speciesSites = new Label[6];
+    
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// Set Choices for choice box
-		species_1.getItems().addAll(treeSpecies);
-		species_2.getItems().addAll(treeSpecies);
-		species_3.getItems().addAll(treeSpecies);
-		species_4.getItems().addAll(treeSpecies);
-		species_5.getItems().addAll(treeSpecies);
-		species_6.getItems().addAll(treeSpecies);
+        // Add species choice boxes to the array
+        speciesChoiceBoxes.add(species_1);
+        speciesChoiceBoxes.add(species_2);
+        speciesChoiceBoxes.add(species_3);
+        speciesChoiceBoxes.add(species_4);
+        speciesChoiceBoxes.add(species_5);
+        speciesChoiceBoxes.add(species_6);
+        
+        // Add the labels to the arrays
+        speciesGroups[0] = species1Group;
+        speciesGroups[1] = species2Group;
+        speciesGroups[2] = species3Group;
+        speciesGroups[3] = species4Group;
+        speciesGroups[4] = species5Group;
+        speciesGroups[5] = species6Group;
+
+        speciesSites[0] = species1Site;
+        speciesSites[1] = species2Site;
+        speciesSites[2] = species3Site;
+        speciesSites[3] = species4Site;
+        speciesSites[4] = species5Site;
+        speciesSites[5] = species6Site;
 		
-	    // Add "Select species" as the default item for each ChoiceBox
-	    species_1.setValue("Select species");
-	    species_2.setValue("Select species");
-	    species_3.setValue("Select species");
-	    species_4.setValue("Select species");
-	    species_5.setValue("Select species");
-	    species_6.setValue("Select species");
-	    
+     // Add "Select species" as the default item for each ChoiceBox and set items
+        for (ChoiceBox<String> choiceBox : speciesChoiceBoxes) {
+            choiceBox.getItems().addAll(treeSpecies);
+            choiceBox.setValue("Select species");
+        }
 
-	    species_1.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
 
-			@Override
-			public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
-				currentSpecies_1 = species_1.getSelectionModel().getSelectedItem();
-			}
-			
-		}); 
+     // Set up listeners for each choice box
+        for (int i = 0; i < speciesChoiceBoxes.size(); i++) {
+            final int index = i; // Need to capture the correct index in the lambda
+
+            speciesChoiceBoxes.get(i).getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+                    // Get the selected item from the choice box at the current index
+                    String currentSpecies = speciesChoiceBoxes.get(index).getSelectionModel().getSelectedItem().substring(0,2);
+
+                    // Update the labels to display the selected item
+                    speciesGroups[index].setText(currentSpecies);
+                    speciesSites[index].setText(currentSpecies);
+                }
+            });
+        }
 	}
-	
 }
 
   
