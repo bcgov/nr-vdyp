ea6acce8457997e6650fdc670c85fe61335ae1ea
Eric Huber <ehuber@vividsolutions.com>
2023-07-25 08:58:33 -0700

    Update menu click

--- vdyp-proofofconcept/src/main/java/application/Main.java
+++ vdyp-proofofconcept/src/main/java/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -12,7 +12,6 @@ import javafx.scene.input.KeyEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
@@ -21,55 +20,58 @@ public class Main extends Application {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
 			Parent root = loader.load();
 			MainController controller = loader.getController();
-			Scene scene = new Scene(root,1000,700);
+			Scene scene = new Scene(root, 1000, 700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
 			// Set up event handler to trigger on shortcut button presses
-	        setupEventHandlers(scene, controller);
-			
-	        // Add icon image and title to the primary stage
-	        setStageIconAndTitle(primaryStage);
-			
+			setupEventHandlers(scene, controller);
+
+			// Add icon image and title to the primary stage
+			setStageIconAndTitle(primaryStage);
+
 			// Show the scene
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
-     * Sets up the event handlers for the specified scene and controller.
-     * The event handler listens for "Ctrl + T" key press to trigger the opening of a secondary window.
-     * 
-     *
-     * @param scene The scene to which the event handler will be attached.
-     * @param controller The MainController instance responsible for handling events.
-     */
+	 * Sets up the event handlers for the specified scene and controller. The event
+	 * handler listens for "Ctrl + T" key press to trigger the opening of a
+	 * secondary window.
+	 *
+	 *
+	 * @param scene      The scene to which the event handler will be attached.
+	 * @param controller The MainController instance responsible for handling
+	 *                   events.
+	 */
 	private void setupEventHandlers(Scene scene, MainController controller) {
-	    scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-	        @Override
-	        public void handle(KeyEvent event) {
-	            if (event.isControlDown() && event.getCode() == KeyCode.T) {
-	                try {
-	                    controller.openSecondaryWindow(event, false); //Setting to false since not trigger by button push
-	                } catch (IOException e) {
-	                    e.printStackTrace();
-	                }
-	            }
-	        }
-	    });
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.isControlDown() && event.getCode() == KeyCode.T) {
+					try {
+						controller.openSecondaryWindow(event, false); // Setting to false since not trigger by button
+																		// push
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 	}
-	
+
 	/**
-     * Sets the icon image and title for the given stage.
-     *
-     * @param stage The stage to which the icon image and title will be set.
-     */
+	 * Sets the icon image and title for the given stage.
+	 *
+	 * @param stage The stage to which the icon image and title will be set.
+	 */
 	private void setStageIconAndTitle(Stage stage) {
-	    Image icon = new Image(getClass().getResource("icon.png").toExternalForm());
-	    stage.getIcons().add(icon);
-	    stage.setTitle("WinVDYP7.9 POC");
+		Image icon = new Image(getClass().getResource("icon.png").toExternalForm());
+		stage.getIcons().add(icon);
+		stage.setTitle("WinVDYP7.9 POC");
 	}
 
 	public static void main(String[] args) {
--- vdyp-proofofconcept/src/main/java/application/SceneController.java
+++ vdyp-proofofconcept/src/main/java/application/SceneController.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.ArrayList;
@@ -13,12 +12,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
-
-
 public class SceneController implements Initializable {
-	
-	//Set up species choice boxes
-	@FXML 
+
+	// Set up species choice boxes
+	@FXML
 	private ChoiceBox<String> species_1;
 	@FXML
 	private ChoiceBox<String> species_2;
@@ -30,104 +27,101 @@ public class SceneController implements Initializable {
 	private ChoiceBox<String> species_5;
 	@FXML
 	private ChoiceBox<String> species_6;
-	
-	// List of speciesChoiceBoxes
-    private List<ChoiceBox<String>> speciesChoiceBoxes = new ArrayList<>();
-	
-	// An array containing the names of different tree species
-	private final String[] treeSpecies = {
-		"AC - Popular", "AT - Aspen", "B - True Fir", "BA - Amabilis Fir","BG - Grand Fir", "BL - Alpine Fir", "CW - Western Red Cedar", "DR - Red Alder",
-		"E - Birch", "EA - Alaska Paper Birch", "EP - Common Paper Birch", "FD - Douglas Fir", "H - Hemlock", "HM - Mountain Hemlock", "HW - Western Hemlock",
-		"L - Larch","LA - Alpine Larch", "LT - Tamarack", "LW - Western Larch", "MB - Bigleaf Maple","PA - Whitebark Pine", "PF - Limber Pine", "PJ - Jack Pine",
-		"PL - Lodgepole Pine", "PY - Western White Pine", "PY - Ponderosa (Yellow) Pine", "S - Spruce", "SB - Black Spruce","SE - Engelmann Spruce", 
-		"SS - Sitka Spruce","SW - White Spruce", "YC - Yellow Cedar"
-							};
 
-	
+	// List of speciesChoiceBoxes
+	private List<ChoiceBox<String>> speciesChoiceBoxes = new ArrayList<>();
+
+	// An array containing the names of different tree species
+	private final String[] treeSpecies = { "AC - Popular", "AT - Aspen", "B - True Fir", "BA - Amabilis Fir",
+			"BG - Grand Fir", "BL - Alpine Fir", "CW - Western Red Cedar", "DR - Red Alder", "E - Birch",
+			"EA - Alaska Paper Birch", "EP - Common Paper Birch", "FD - Douglas Fir", "H - Hemlock",
+			"HM - Mountain Hemlock", "HW - Western Hemlock", "L - Larch", "LA - Alpine Larch", "LT - Tamarack",
+			"LW - Western Larch", "MB - Bigleaf Maple", "PA - Whitebark Pine", "PF - Limber Pine", "PJ - Jack Pine",
+			"PL - Lodgepole Pine", "PY - Western White Pine", "PY - Ponderosa (Yellow) Pine", "S - Spruce",
+			"SB - Black Spruce", "SE - Engelmann Spruce", "SS - Sitka Spruce", "SW - White Spruce",
+			"YC - Yellow Cedar" };
+
 	// Labels for display based on choice boxes
 	@FXML
-    private Label species1Group;
-    @FXML
-    private Label species1Site;
-    @FXML
-    private Label species2Group;
-    @FXML
-    private Label species2Site;
-    @FXML
-    private Label species3Group;
-    @FXML
-    private Label species3Site;
-    @FXML
-    private Label species4Group;
-    @FXML
-    private Label species4Site;
-    @FXML
-    private Label species5Group;
-    @FXML
-    private Label species5Site;
-    @FXML
-    private Label species6Group;
-    @FXML
-    private Label species6Site;
-    
- 
-    // Array of Species Groups and Sites
-    @FXML
-    private Label[] speciesGroups = new Label[6];
-    @FXML
-    private Label[] speciesSites = new Label[6];
-    
+	private Label species1Group;
+	@FXML
+	private Label species1Site;
+	@FXML
+	private Label species2Group;
+	@FXML
+	private Label species2Site;
+	@FXML
+	private Label species3Group;
+	@FXML
+	private Label species3Site;
+	@FXML
+	private Label species4Group;
+	@FXML
+	private Label species4Site;
+	@FXML
+	private Label species5Group;
+	@FXML
+	private Label species5Site;
+	@FXML
+	private Label species6Group;
+	@FXML
+	private Label species6Site;
+
+	// Array of Species Groups and Sites
+	@FXML
+	private Label[] speciesGroups = new Label[6];
+	@FXML
+	private Label[] speciesSites = new Label[6];
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-        // Add species choice boxes to the array
-        speciesChoiceBoxes.add(species_1);
-        speciesChoiceBoxes.add(species_2);
-        speciesChoiceBoxes.add(species_3);
-        speciesChoiceBoxes.add(species_4);
-        speciesChoiceBoxes.add(species_5);
-        speciesChoiceBoxes.add(species_6);
-        
-        // Add the labels to the arrays
-        speciesGroups[0] = species1Group;
-        speciesGroups[1] = species2Group;
-        speciesGroups[2] = species3Group;
-        speciesGroups[3] = species4Group;
-        speciesGroups[4] = species5Group;
-        speciesGroups[5] = species6Group;
+		// Add species choice boxes to the array
+		speciesChoiceBoxes.add(species_1);
+		speciesChoiceBoxes.add(species_2);
+		speciesChoiceBoxes.add(species_3);
+		speciesChoiceBoxes.add(species_4);
+		speciesChoiceBoxes.add(species_5);
+		speciesChoiceBoxes.add(species_6);
 
-        speciesSites[0] = species1Site;
-        speciesSites[1] = species2Site;
-        speciesSites[2] = species3Site;
-        speciesSites[3] = species4Site;
-        speciesSites[4] = species5Site;
-        speciesSites[5] = species6Site;
-		
-     // Add "Select species" as the default item for each ChoiceBox and set items
-        for (ChoiceBox<String> choiceBox : speciesChoiceBoxes) {
-            choiceBox.getItems().addAll(treeSpecies);
-            choiceBox.setValue("Select species");
-        }
+		// Add the labels to the arrays
+		speciesGroups[0] = species1Group;
+		speciesGroups[1] = species2Group;
+		speciesGroups[2] = species3Group;
+		speciesGroups[3] = species4Group;
+		speciesGroups[4] = species5Group;
+		speciesGroups[5] = species6Group;
 
+		speciesSites[0] = species1Site;
+		speciesSites[1] = species2Site;
+		speciesSites[2] = species3Site;
+		speciesSites[3] = species4Site;
+		speciesSites[4] = species5Site;
+		speciesSites[5] = species6Site;
 
-     // Set up listeners for each choice box
-        for (int i = 0; i < speciesChoiceBoxes.size(); i++) {
-            final int index = i; // Need to capture the correct index in the lambda
+		// Add "Select species" as the default item for each ChoiceBox and set items
+		for (ChoiceBox<String> choiceBox : speciesChoiceBoxes) {
+			choiceBox.getItems().addAll(treeSpecies);
+			choiceBox.setValue("Select species");
+		}
 
-            speciesChoiceBoxes.get(i).getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-                @Override
-                public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
-                    // Get the selected item from the choice box at the current index
-                    String currentSpecies = speciesChoiceBoxes.get(index).getSelectionModel().getSelectedItem().substring(0,2);
+		// Set up listeners for each choice box
+		for (int i = 0; i < speciesChoiceBoxes.size(); i++) {
+			final int index = i; // Need to capture the correct index in the lambda
 
-                    // Update the labels to display the selected item
-                    speciesGroups[index].setText(currentSpecies);
-                    speciesSites[index].setText(currentSpecies);
-                }
-            });
-        }
+			speciesChoiceBoxes.get(i).getSelectionModel().selectedItemProperty()
+					.addListener(new ChangeListener<String>() {
+						@Override
+						public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+							// Get the selected item from the choice box at the current index
+							String currentSpecies = speciesChoiceBoxes.get(index).getSelectionModel().getSelectedItem()
+									.substring(0, 2);
+
+							// Update the labels to display the selected item
+							speciesGroups[index].setText(currentSpecies);
+							speciesSites[index].setText(currentSpecies);
+						}
+					});
+		}
 	}
-	
-	
-}
 
-  
+}
