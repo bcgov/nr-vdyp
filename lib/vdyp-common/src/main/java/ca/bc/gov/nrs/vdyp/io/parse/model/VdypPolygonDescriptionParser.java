999597d475739e38f89dd4d0c45c29ad0b6f6f1b
Kevin Smith <ksmith@vividsolutions.com>
2024-10-23 13:12:51 -0700

    Unit test for polygon ID parser

--- lib/vdyp-common/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/model/VdypPolygonDescriptionParser.java
+++ lib/vdyp-common/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/model/VdypPolygonDescriptionParser.java
@@ -33,6 +33,10 @@ public class VdypPolygonDescriptionParser implements ControlMapValueReplacer<Obj
 		Integer year;
 		String name;
 
+		if (description.length() != PolygonIdentifier.ID_LENGTH) {
+			throw new ResourceParseException("Polygon description " + description + " was not 25 characters long");
+		}
+
 		if (matcher.matches() && matcher.group(2) != null) {
 			year = Integer.parseInt(matcher.group(2));
 			name = matcher.group(1).trim();
