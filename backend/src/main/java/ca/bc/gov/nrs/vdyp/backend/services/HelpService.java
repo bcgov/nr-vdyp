e277a6bb94ad7eb97b64524007590a80d35bd305
Michael Junkin <michael.junkin@gmail.com>
2025-05-08 15:17:03 -0700

    Yield table testing framework

--- backend/src/main/java/ca/bc/gov/nrs/vdyp/backend/services/HelpService.java
+++ backend/src/main/java/ca/bc/gov/nrs/vdyp/backend/services/HelpService.java
@@ -448,8 +448,7 @@ public class HelpService {
 						"selectedExecutionOptions." + field, //
 						"Allow " + shortDescription, //
 						"Enables " + longDescription, //
-						ValidatedParameters.DEFAULT.containsOption(executionOption) ? "Selected"
-								: "Not Selected"
+						ValidatedParameters.DEFAULT.containsOption(executionOption) ? "Selected" : "Not Selected"
 				)
 		);
 
@@ -458,8 +457,7 @@ public class HelpService {
 						"excludedExecutionOptions." + field, //
 						"Disallow " + shortDescription, //
 						"Disables " + longDescription, //
-						ValidatedParameters.DEFAULT.containsOption(executionOption)
-								? "Not Excluded" : "Excluded"
+						ValidatedParameters.DEFAULT.containsOption(executionOption) ? "Not Excluded" : "Excluded"
 				)
 		);
 	}
