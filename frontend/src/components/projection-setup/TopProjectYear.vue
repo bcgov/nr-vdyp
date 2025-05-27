41dae1780a38ce49ce4710440dbce2dc6137b2c0
Roy Jeong <roy.jeong@gov.bc.ca>
2025-05-27 13:13:44 -0700

    VDYP-622: Fiscal Year on main UI should be updated to 2025/2026 and should adjust automatically

--- frontend/src/components/projection-setup/TopProjectYear.vue
+++ frontend/src/components/projection-setup/TopProjectYear.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="top-project-year mt-3">
     <h1 class="top-project">{{ title }}</h1>
-    <span class="top-year">Year: {{ year }}</span>
+    <span class="top-year">Year: {{ fiscalYear }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
 defineProps({
   title: {
     type: String,
     default: 'Projects',
   },
-  year: {
-    type: String,
-    default: '2024/2025',
-  },
+})
+
+const fiscalYear = ref('2024/2025')
+
+const calculateFiscalYear = (date: Date): string => {
+  const year = date.getFullYear()
+  const month = date.getMonth() // 0-11 (January is 0, March is 2, April is 3)
+
+  // Fiscal year changes on April 1st
+  if (month >= 3) {
+    // April (3) or later
+    return `${year}/${year + 1}`
+  } else {
+    return `${year - 1}/${year}`
+  }
+}
+
+onMounted(() => {
+  const currentDate = new Date()
+  fiscalYear.value = calculateFiscalYear(currentDate)
 })
 </script>
 <style scoped>
