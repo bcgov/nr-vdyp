ab9cb48c5b0091bdfb8340ee8d79ce353c3518f3
Peter Minter <pminter@vividsolutions.com>
2025-06-06 15:08:44 -0700

    Add coverage to the uncallable age Year Combination in context

--- backend/src/test/java/ca/bc/gov/nrs/vdyp/backend/projection/output/yieldtable/YieldTableRowContextTest.java
+++ backend/src/test/java/ca/bc/gov/nrs/vdyp/backend/projection/output/yieldtable/YieldTableRowContextTest.java
@@ -1,14 +1,10 @@
 package ca.bc.gov.nrs.vdyp.backend.projection.output.yieldtable;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import java.util.ArrayList;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -18,141 +14,145 @@ import ca.bc.gov.nrs.vdyp.backend.model.v1.Parameters;
 import ca.bc.gov.nrs.vdyp.backend.model.v1.ProjectionRequestKind;
 import ca.bc.gov.nrs.vdyp.backend.projection.PolygonProjectionState;
 import ca.bc.gov.nrs.vdyp.backend.projection.ProjectionContext;
-import ca.bc.gov.nrs.vdyp.backend.projection.model.Layer;
-import ca.bc.gov.nrs.vdyp.backend.projection.model.LayerReportingInfo;
+import ca.bc.gov.nrs.vdyp.backend.projection.ValidatedParameters;
 import ca.bc.gov.nrs.vdyp.backend.projection.model.Polygon;
-import ca.bc.gov.nrs.vdyp.backend.projection.model.Species;
-import ca.bc.gov.nrs.vdyp.backend.projection.model.SpeciesReportingInfo;
-import ca.bc.gov.nrs.vdyp.backend.projection.model.Stand;
-import ca.bc.gov.nrs.vdyp.si32.site.SiteTool;
 
 public class YieldTableRowContextTest {
-    Parameters params;
-    ProjectionContext context;
-    Polygon polygon;
-    PolygonProjectionState state;
-    Layer layer;
-    LayerReportingInfo layerInfo;
-    @BeforeEach
-    void setup() throws AbstractProjectionRequestException {
-        params = new Parameters().ageStart(0).ageEnd(100);
-        polygon = new Polygon.Builder().build();
-        context = new ProjectionContext(ProjectionRequestKind.HCSV, "TEST", params, false);
-        state = new PolygonProjectionState();
-        layer = new Layer.Builder().layerId("1").polygon(polygon).build();
-        Stand stand = new Stand.Builder().layer(layer).sp0Code("PL").build();
-        layer.addStand(stand);
-        Species species1 = new Species.Builder().stand(stand).speciesCode("PL").speciesPercent(60.0).build();
-        layer.addSp64(species1);
-        layerInfo = new LayerReportingInfo.Builder().layer(layer).build();
-    }
 
-    @Test
-    void testSortedSpeciesFromLayerReportingInfo() {
-        YieldTableRowContext.of(context, polygon, state, layerInfo);
-    }
+	static class AgeYearCombo {
+		private AgeYearCombo(Parameters.AgeYearRangeCombinationKind k, Integer as, Integer ae, Integer ys, Integer ye) {
+			ageStart = as;
+			ageEnd = ae;
+			yearStart = ys;
+			yearEnd = ye;
+			kind = k;
+		}
 
-    static class AgeYearCombo{
-        private AgeYearCombo(Parameters.AgeYearRangeCombinationKind k, Integer as, Integer ae, Integer ys, Integer ye){
-            ageStart = as;
-            ageEnd = ae;
-            yearStart = ys;
-            yearEnd = ye;
-            kind = k;
-        }
-        static AgeYearCombo of(Parameters.AgeYearRangeCombinationKind kind, Integer ageStart, Integer ageEnd, Integer yearStart, Integer yearEnd){
-            return new AgeYearCombo(kind, ageStart, ageEnd, yearStart, yearEnd);
-        }
-        Parameters.AgeYearRangeCombinationKind kind;
-        final Integer ageStart;
-        final Integer ageEnd;
-        final Integer yearStart;
-        final Integer yearEnd;
-    }
-    static class AgeYearComboResults{
-        private AgeYearComboResults(Integer ays, Integer aye, Integer ags, Integer age){
-            ageAtStartYear = ays;
-            ageAtEndYear = aye;
-            ageAtGapStart = ags;
-            ageAtGapEnd = age;
-        }
-        static AgeYearComboResults of(Integer ageAtStartYear, Integer ageAtEndYear, Integer ageAtGapStart, Integer ageAtGapEnd){
-            return new AgeYearComboResults(ageAtStartYear, ageAtEndYear, ageAtGapStart, ageAtGapEnd);
-        }
-        final Integer ageAtStartYear;
-        final Integer ageAtEndYear;
-        final Integer ageAtGapStart;
-        final Integer ageAtGapEnd;
-    }
+		static AgeYearCombo of(
+				Parameters.AgeYearRangeCombinationKind kind, Integer ageStart, Integer ageEnd, Integer yearStart,
+				Integer yearEnd
+		) {
+			return new AgeYearCombo(kind, ageStart, ageEnd, yearStart, yearEnd);
+		}
 
-    static Stream<Arguments> ageYearCombinations(){
-        return Stream.of(
-                /*
-                Arguments.of(AgeYearCombo.of(null, 0,100,null,null),
-                        AgeYearComboResults.of(0,100,null,null)),
-                Arguments.of(AgeYearCombo.of(null, null,null,2000,2100),
-                        AgeYearComboResults.of(50,150,null,null)),
-                Arguments.of(AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.INTERSECT, 0,100,2000,2100),// Overlapping Intersection
-                        AgeYearComboResults.of(50,100,null,null)),
-                Arguments.of(AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.INTERSECT, 0,40,2000,2100), // Disjoint Intersection
-                        AgeYearComboResults.of(null,null,null,null)),*/
-                Arguments.of(AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.UNION, 0,100,2000,2100), // Overlapping Union
-                        AgeYearComboResults.of(0,150,null,null)),
-                Arguments.of(AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.UNION, 0,40,2000,2100), // Disjoint Union
-                        AgeYearComboResults.of(0,150,40,50))
-        );
-    }
+		Parameters.AgeYearRangeCombinationKind kind;
+		final Integer ageStart;
+		final Integer ageEnd;
+		final Integer yearStart;
+		final Integer yearEnd;
+	}
 
+	static class AgeYearComboResults {
+		private AgeYearComboResults(Integer ays, Integer aye, Integer ags, Integer age) {
+			ageAtStartYear = ays;
+			ageAtEndYear = aye;
+			ageAtGapStart = ags;
+			ageAtGapEnd = age;
+		}
 
-    @ParameterizedTest
-    @MethodSource("ageYearCombinations")
-    void testAgeYearCombinations(AgeYearCombo ageYearParams, AgeYearComboResults results) throws AbstractProjectionRequestException {
-        params = new Parameters().ageStart(ageYearParams.ageStart).ageEnd(ageYearParams.ageEnd).yearStart(ageYearParams.yearStart).yearEnd(ageYearParams.yearEnd).combineAgeYearRange(ageYearParams.kind);
-        polygon = new Polygon.Builder().referenceYear(1950).build();
-        context = new ProjectionContext(ProjectionRequestKind.HCSV, "TEST", params, false);
-        state = new PolygonProjectionState();
-        layer = new Layer.Builder().layerId("1").polygon(polygon).build();
-        var sp64Code = "PL";
-        var sp0Code = SiteTool.getSpeciesVDYP7Code(sp64Code);
+		static AgeYearComboResults
+				of(Integer ageAtStartYear, Integer ageAtEndYear, Integer ageAtGapStart, Integer ageAtGapEnd) {
+			return new AgeYearComboResults(ageAtStartYear, ageAtEndYear, ageAtGapStart, ageAtGapEnd);
+		}
 
-        var stand = new Stand.Builder() //
-                .sp0Code("P") //
-                .layer(layer) //
-                .build();
+		final Integer ageAtStartYear;
+		final Integer ageAtEndYear;
+		final Integer ageAtGapStart;
+		final Integer ageAtGapEnd;
+	}
 
-        var sp64 = new Species.Builder() //
-                .stand(stand) //
-                .speciesCode(sp64Code) //
-                .speciesPercent(100d) //
-                .totalAge(40d) //
-                .dominantHeight(20d) //
-                .build();
+	static Stream<Arguments> ageYearCombinations() {
+		return Stream.of(
 
-        Species sp0 = new Species.Builder().stand(stand) //
-                .speciesCode(sp64Code) //
-                .speciesPercent(0) //
-                .build();
-        stand.addSpeciesGroup(sp0, layer.getSp0sAsSupplied().size());
-        layer.addStand(stand);
+				Arguments.of(AgeYearCombo.of(null, 0, 100, null, null), AgeYearComboResults.of(0, 100, null, null)),
+				Arguments
+						.of(AgeYearCombo.of(null, null, null, 2000, 2100), AgeYearComboResults.of(50, 150, null, null)),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.INTERSECT, 0, 100, 2000, 2100), //
+						AgeYearComboResults.of(50, 100, null, null)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.INTERSECT, 0, 40, 2000, 2100), //
+						AgeYearComboResults.of(null, null, null, null)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.UNION, 0, 100, 2000, 2100), //
+						AgeYearComboResults.of(0, 150, null, null)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.UNION, 0, 40, 2000, 2100), //
+						AgeYearComboResults.of(0, 150, 40, 50)
+				),
 
-        sp64.calculateUndefinedFieldValues();
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.DIFFERENCE, 50, 100, 1950, 2100), // Overlapping
+						// Difference
+						AgeYearComboResults.of(0, 150, 49, 101)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.DIFFERENCE, 0, 100, 1950, 2050), // Overlapping
+						// Difference
+						AgeYearComboResults.of(null, null, null, null)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.DIFFERENCE, 0, 150, 1975, 2050), // Overlapping
+						// Difference
+						AgeYearComboResults.of(0, 150, 24, 101)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.DIFFERENCE, 0, 100, 2000, 2100), // Overlapping
+						// Difference
+						AgeYearComboResults.of(0, 150, 49, 101)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.DIFFERENCE, 0, 40, 2000, 2100), // Disjoint
+						// Difference
+						AgeYearComboResults.of(0, 150, 40, 50)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.DIFFERENCE, 75, 100, 1950, 2000), // Disjoint
+						// Difference
+						AgeYearComboResults.of(0, 100, 50, 75)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.DIFFERENCE, 0, 150, 1950, 2000), // Disjoint
+						// Difference
+						AgeYearComboResults.of(51, 150, null, null)
+				),
+				Arguments.of(
+						AgeYearCombo.of(Parameters.AgeYearRangeCombinationKind.DIFFERENCE, 0, 50, 1975, 2000), // Disjoint
+						// Difference
+						AgeYearComboResults.of(0, 24, null, null)
+				)
+		);
+	}
 
-        stand.addSp64(sp64);
-        layer.addSp64(sp64);
+	@ParameterizedTest
+	@MethodSource("ageYearCombinations")
+	void testAgeYearCombinations(AgeYearCombo ageYearParams, AgeYearComboResults results)
+			throws AbstractProjectionRequestException {
 
-        var speciesReportingInfo = new SpeciesReportingInfo.Builder() //
-                .sp64Name(sp64Code) //
-                .sp64Percent(100d) //
-                .asSuppliedIndex(0) //
-                .build();
-        layerInfo = new LayerReportingInfo.Builder().layer(layer).build();
-        ArrayList<SpeciesReportingInfo> list = new ArrayList<SpeciesReportingInfo>();
-        list.add(speciesReportingInfo);
-        layerInfo.setSpeciesReportingInfos(list);
-        var yieldTableRowContext = YieldTableRowContext.of(context, polygon, state, layerInfo);
-        assertThat(yieldTableRowContext.getAgeAtStartYear(), is(results.ageAtStartYear));
-        assertThat(yieldTableRowContext.getAgeAtEndYear(), is(results.ageAtEndYear));
-        assertThat(yieldTableRowContext.getAgeAtGapStart(), is(results.ageAtGapStart));
-        assertThat(yieldTableRowContext.getAgeAtGapEnd(), is(results.ageAtGapEnd));
-    }
+		var params = new Parameters().ageStart(ageYearParams.ageStart).ageEnd(ageYearParams.ageEnd)
+				.yearStart(ageYearParams.yearStart).yearEnd(ageYearParams.yearEnd);
+
+		ValidatedParameters vp = new ValidatedParameters().ageStart(ageYearParams.ageStart).ageEnd(ageYearParams.ageEnd)
+				.yearStart(ageYearParams.yearStart).yearEnd(ageYearParams.yearEnd);
+		vp.setCombineAgeYearRange(ageYearParams.kind);
+
+		var polygon = new Polygon.Builder().referenceYear(1950).build();
+		var context = new ProjectionContext(ProjectionRequestKind.HCSV, "TEST", params, false) {
+			// Overriding because parameter validation does not allow for these other kinds of Age YEar Combos despite
+			@Override
+			public ValidatedParameters getParams() {
+				return vp;
+			}
+		};
+		var state = new PolygonProjectionState();
+
+		var yieldTableRowContext = YieldTableRowContext.of(context, polygon, state, null);
+		assertThat(yieldTableRowContext.getAgeAtStartYear(), is(results.ageAtStartYear));
+		assertThat(yieldTableRowContext.getAgeAtEndYear(), is(results.ageAtEndYear));
+		assertThat(yieldTableRowContext.getAgeAtGapStart(), is(results.ageAtGapStart));
+		assertThat(yieldTableRowContext.getAgeAtGapEnd(), is(results.ageAtGapEnd));
+	}
 }
