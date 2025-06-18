799e1ea051226e874d23a136f541cb0904a94de7
Peter Minter <pminter@vividsolutions.com>
2025-06-18 15:44:44 -0700

    Remove unused errorNumber Parameter, was handled by getIpassCode in child classes

--- lib/vdyp-common/src/main/java/ca/bc/gov/nrs/vdyp/exceptions/ProcessingException.java
+++ lib/vdyp-common/src/main/java/ca/bc/gov/nrs/vdyp/exceptions/ProcessingException.java
@@ -1,8 +1,5 @@
 package ca.bc.gov.nrs.vdyp.exceptions;
 
-import java.text.MessageFormat;
-import java.util.Optional;
-
 /**
  * A problem occurred while VDYP was processing data
  *
@@ -13,39 +10,24 @@ public class ProcessingException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
-	private final Optional<Integer> errorNumber;
-
 	public ProcessingException() {
 		super();
-		this.errorNumber = Optional.empty();
 	}
 
 	public ProcessingException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		this.errorNumber = Optional.empty();
-	}
-
-	public ProcessingException(String message, int errorNumber) {
-		super(MessageFormat.format("{0} ({1})", message, errorNumber));
-		this.errorNumber = Optional.of(errorNumber);
 	}
 
 	public ProcessingException(String message, Throwable cause) {
 		super(message, cause);
-		this.errorNumber = Optional.empty();
 	}
 
 	public ProcessingException(String message) {
 		super(message);
-		this.errorNumber = Optional.empty();
 	}
 
 	public ProcessingException(Throwable cause) {
 		super(cause);
-		this.errorNumber = Optional.empty();
 	}
 
-	public Optional<Integer> getErrorNumber() {
-		return errorNumber;
-	}
 }
