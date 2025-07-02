943049dbd864f5c111cb78e7867a21fae9f4dec1
Peter Minter <pminter@vividsolutions.com>
2025-07-02 15:24:20 -0700

    Replace Identical coefficients with EMPTY static records

--- lib/vdyp-si32/src/main/java/ca/bc/gov/nrs/vdyp/si32/cfs/CfsBiomassConversionCoefficientsDetails.java
+++ lib/vdyp-si32/src/main/java/ca/bc/gov/nrs/vdyp/si32/cfs/CfsBiomassConversionCoefficientsDetails.java
@@ -3,7 +3,34 @@ package ca.bc.gov.nrs.vdyp.si32.cfs;
 import java.text.MessageFormat;
 import java.util.Arrays;
 
+// Consider a record with named values for better clarity. It saves almost nothing to have the params be an array of loats they are never different.
+// We also don't need to access them based on enum offsets this
 public record CfsBiomassConversionCoefficientsDetails(boolean containsData, float[] parms) {
+
+	public static final CfsBiomassConversionCoefficientsDetails LIVE_EMPTY = new CfsBiomassConversionCoefficientsDetails(
+			false,
+			new float[] { -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f,
+					-9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f,
+					-9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f,
+					-9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f,
+					-9.00000000f, -9.00000000f }
+
+	);
+
+	public static final CfsBiomassConversionCoefficientsDetails DEAD_EMPTY = new CfsBiomassConversionCoefficientsDetails(
+			false,
+			new float[] { -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f, -9.00000000f,
+					-9.00000000f, -9.00000000f, -9.00000000f }
+	);
+
+	public float getParm(CfsLiveConversionParams parm) {
+		return parms[parm.getIndex()];
+	}
+
+	public float getParm(CfsDeadConversionParams parm) {
+		return parms[parm.getIndex()];
+	}
+
 	@Override
 	public String toString() {
 		return MessageFormat.format("ContainsData: {0}; parms: {1}", containsData, Arrays.toString(parms));
