b5989641268f29742e4b3256ef2a7ce929c0e115
Peter Minter <pminter@vividsolutions.com>
2025-07-03 10:30:33 -0700

    Add tests for getting genera from species

--- lib/vdyp-si32/src/test/java/ca/bc/gov/nrs/vdyp/si32/CfsBioMassConversionCoefficientsTests.java
+++ lib/vdyp-si32/src/test/java/ca/bc/gov/nrs/vdyp/si32/CfsBioMassConversionCoefficientsTests.java
@@ -1,15 +1,24 @@
 package ca.bc.gov.nrs.vdyp.si32;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.Stream;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import ca.bc.gov.nrs.vdyp.si32.cfs.CfsBiomassConversionCoefficientsDead;
 import ca.bc.gov.nrs.vdyp.si32.cfs.CfsBiomassConversionCoefficientsDetails;
 import ca.bc.gov.nrs.vdyp.si32.cfs.CfsBiomassConversionCoefficientsForGenus;
 import ca.bc.gov.nrs.vdyp.si32.cfs.CfsBiomassConversionCoefficientsForSpecies;
+import ca.bc.gov.nrs.vdyp.si32.cfs.CfsBiomassConversionSupportedEcoZone;
+import ca.bc.gov.nrs.vdyp.si32.cfs.CfsBiomassConversionSupportedGenera;
 
 class CfsBioMassConversionCoefficientsTests {
 
@@ -44,5 +53,54 @@ class CfsBioMassConversionCoefficientsTests {
 		assertFalse(d1.equals(d2));
 		assertThat(d1.toString(), Matchers.notNullValue());
 		assertTrue(d1.hashCode() == d1.hashCode());
+	}
+
+	static Stream<Arguments> cfsGeneraByEcoZoneAndSpecies() {
+		return Stream.of(
+				Arguments.of(
+						CfsBiomassConversionSupportedEcoZone.UNKNOWN, "A", CfsBiomassConversionSupportedGenera.INVALID
+				),
+				Arguments.of(
+						CfsBiomassConversionSupportedEcoZone.TAIGA_PLAINS, "BAD",
+						CfsBiomassConversionSupportedGenera.INVALID
+				),
+				Arguments.of(
+						CfsBiomassConversionSupportedEcoZone.BOREAL_CORDILLERA, "A",
+						CfsBiomassConversionSupportedGenera.INVALID
+				),
+				Arguments.of(
+						CfsBiomassConversionSupportedEcoZone.BOREAL_CORDILLERA, "A",
+						CfsBiomassConversionSupportedGenera.INVALID
+				),
+				Arguments.of(
+						CfsBiomassConversionSupportedEcoZone.MONTANE_CORDILLERA, "SXW",
+						CfsBiomassConversionSupportedGenera.S
+				),
+				Arguments.of(
+						CfsBiomassConversionSupportedEcoZone.BOREAL_CORDILLERA, "SWX",
+						CfsBiomassConversionSupportedGenera.INVALID
+				),
+				Arguments.of(
+						CfsBiomassConversionSupportedEcoZone.BOREAL_CORDILLERA, "B",
+						CfsBiomassConversionSupportedGenera.B
+				),
+				Arguments.of(
+						CfsBiomassConversionSupportedEcoZone.BOREAL_CORDILLERA, "C",
+						CfsBiomassConversionSupportedGenera.C
+				),
+				Arguments.of(
+						CfsBiomassConversionSupportedEcoZone.BOREAL_CORDILLERA, "D",
+						CfsBiomassConversionSupportedGenera.D
+				)
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("cfsGeneraByEcoZoneAndSpecies")
+	void testGetCfsBiomassConversionGenera(
+			CfsBiomassConversionSupportedEcoZone ecoZone, String sp64, CfsBiomassConversionSupportedGenera expectedGenus
+	) {
+
+		assertThat(CfsBiomassConversionSupportedGenera.fromEcoZoneAndSpecies(ecoZone, sp64), is(expectedGenus));
 	}
 }
