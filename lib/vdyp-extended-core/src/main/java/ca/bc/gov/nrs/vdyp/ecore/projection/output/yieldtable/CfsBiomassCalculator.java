f64b3ce18f93e6e9b5d98427a337da69f073473d
Peter Minter <pminter@vividsolutions.com>
2025-07-07 13:54:37 -0700

    Add Cfs biomass calculator class

---
+++ lib/vdyp-extended-core/src/main/java/ca/bc/gov/nrs/vdyp/ecore/projection/output/yieldtable/CfsBiomassCalculator.java
@@ -0,0 +1,386 @@
+package ca.bc.gov.nrs.vdyp.ecore.projection.output.yieldtable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ca.bc.gov.nrs.vdyp.ecore.model.v1.MessageSeverityCode;
+import ca.bc.gov.nrs.vdyp.ecore.model.v1.PolygonMessageKind;
+import ca.bc.gov.nrs.vdyp.ecore.projection.model.Layer;
+import ca.bc.gov.nrs.vdyp.ecore.projection.model.Polygon;
+import ca.bc.gov.nrs.vdyp.ecore.projection.model.PolygonMessage;
+import ca.bc.gov.nrs.vdyp.ecore.projection.model.Species;
+import ca.bc.gov.nrs.vdyp.ecore.projection.model.enumerations.ReturnCode;
+import ca.bc.gov.nrs.vdyp.si32.cfs.CfsBiomassConversionCoefficientsDetails;
+import ca.bc.gov.nrs.vdyp.si32.cfs.CfsBiomassConversionSupportedEcoZone;
+import ca.bc.gov.nrs.vdyp.si32.cfs.CfsLiveConversionParams;
+import ca.bc.gov.nrs.vdyp.si32.site.SiteTool;
+
+/**
+ * Calculates CFS Biomass Volume Details based on the provided EntityVolumeDetails (Polygon or Layer) for either an
+ * entire polygon or a specific layer.
+ *
+ */
+public class CfsBiomassCalculator {
+	private static final Logger logger = LoggerFactory.getLogger(SiteTool.class);
+
+	/**
+	 * Calculates CFS Biomass Volume Details for a Polygon.
+	 *
+	 * @param volumeData The EntityVolumeDetails containing the raw volume data for a year.
+	 * @param polygon    The Polygon for which the biomass is calculated.
+	 * @return CfsBiomassVolumeDetails containing the calculated biomass details.
+	 */
+	public static CfsBiomassVolumeDetails
+			calculateBiomassPolygonVolumeDetails(EntityVolumeDetails volumeData, Polygon polygon) {
+		CfsBiomassConversionSupportedEcoZone ecoZone = getEcoZoneForPolygon(polygon);
+		String mofSp64Code = getSp64Code(polygon, null);
+		return calculateCfsBiomass(volumeData, ecoZone, mofSp64Code, polygon);
+	}
+
+	/**
+	 * Calculates CFS Biomass Volume Details for a specific Layer within a Polygon.
+	 *
+	 * @param volumeData The EntityVolumeDetails containing the raw volume data for a year.
+	 * @param polygon    The Polygon containing the layer.
+	 * @param layer      The Layer for which the biomass is calculated.
+	 * @return CfsBiomassVolumeDetails containing the calculated biomass details.
+	 */
+	public static CfsBiomassVolumeDetails
+			calculateBiomassLayerVolumeDetails(EntityVolumeDetails volumeData, Polygon polygon, Layer layer) {
+		CfsBiomassConversionSupportedEcoZone ecoZone = getEcoZoneForPolygon(polygon);
+		if (layer.getIsDeadLayer()) {
+			logger.error("Layer is a dead layer. Cannot calculate CFS Biomass for dead layers.");
+			return CfsBiomassVolumeDetails.EMPTY;
+		}
+		String mofSp64Code = getSp64Code(polygon, layer);
+		return calculateCfsBiomass(volumeData, ecoZone, mofSp64Code, polygon);
+	}
+
+	/**
+	 * Determines the CFS EcoZone for a given Polygon.
+	 *
+	 * @param polygon The Polygon for which the EcoZone is determined.
+	 * @return The CfsBiomassConversionSupportedEcoZone corresponding to the Polygon's EcoZone or BEC zone.
+	 */
+	public static CfsBiomassConversionSupportedEcoZone getEcoZoneForPolygon(Polygon polygon) {
+		CfsBiomassConversionSupportedEcoZone ecoZone = CfsBiomassConversionSupportedEcoZone.UNKNOWN;
+		if (polygon == null) {
+			logger.warn("Polygon is null. Cannot determine EcoZone.");
+			return ecoZone;
+		}
+		if (polygon.getCfsEcoZone() != null) {
+			ecoZone = CfsBiomassConversionSupportedEcoZone.of(polygon.getCfsEcoZone().getCode());
+			if (ecoZone != CfsBiomassConversionSupportedEcoZone.UNKNOWN) {
+				logger.debug("Using CFS EcoZone from Polygon: {}", ecoZone);
+			}
+		}
+		if (ecoZone == CfsBiomassConversionSupportedEcoZone.UNKNOWN) {
+
+			logger.debug("Attempt to convert BEC zone to CFS EcoZone for Polygon: {}", polygon.getBecZone());
+			ecoZone = CfsBiomassConversionSupportedEcoZone
+					.fromBecZoneData(polygon.getBecZone(), null, null, polygon.getMapSheet());
+			if (ecoZone != CfsBiomassConversionSupportedEcoZone.UNKNOWN) {
+				logger.debug(
+						"CFS EcoZone determined from BEC zone {} and MapSheet {}: {}", polygon.getBecZone(),
+						polygon.getMapSheet(), ecoZone
+				);
+			} else {
+				logger.error(
+						"CFS EcoZone could not be determined for Polygon: {}. Using UNKNOWN EcoZone.",
+						polygon.getBecZone()
+				);
+			}
+		}
+		return ecoZone;
+	}
+
+	public static String getSp64Code(Polygon polygon, Layer layer) {
+		String sp64Code = null;
+		if (layer == null && polygon != null) {
+			layer = polygon.getPrimaryLayer();
+		}
+		if (layer != null) {
+			Species species = layer.determineLeadingSp64(0);
+			sp64Code = species != null ? species.getSpeciesCode() : null;
+		} else {
+			logger.error("Could not determine a primary layer for Polygon: {}. Cannot determine SP64 code.", polygon);
+		}
+		return sp64Code;
+	}
+
+	enum CfsBioProportionToUse {
+		UNKNOWN, LOW_BOUND, COMPUTED, HIGH_BOUND
+	}
+
+	/**
+	 * Calculates CFS Biomass Volume Details based on the provided EntityVolumeDetails, EcoZone, and species code.
+	 * Currently in VDYP7 all calculations are performed for daead biomass proportions but then the results are
+	 * suppressed. For the time being the dead calculations will not run in this version.
+	 *
+	 * @param volumeData EntityVolumeData that contains volume details for the layer or polygon
+	 * @param ecoZone    The CfsBiomassConversionSupportedEcoZone for which the biomass is calculated.
+	 * @param sp64Code   The species code used to look up conversion parameters.
+	 * @param polygon    The polygon in case and messages need to be written to it
+	 * @return CfsBiomassVolumeDetails containing the calculated biomass details.
+	 */
+	public static CfsBiomassVolumeDetails calculateCfsBiomass(
+			EntityVolumeDetails volumeData, CfsBiomassConversionSupportedEcoZone ecoZone, String sp64Code,
+			Polygon polygon
+	) {
+
+		CfsBiomassConversionCoefficientsDetails liveParams = SiteTool.lookupLiveCfsConversionParams(ecoZone, sp64Code);
+		// Commented out because dead biomass is suppressed in the end in VDYP7
+		// CfsBiomassConversionCoefficientsDetails deadParams = SiteTool.lookupDeadCfsConversionParams(ecoZone,
+		// sp64Code);
+		if (liveParams == null || !liveParams.containsData()) {
+			logger.warn(
+					"CFS Biomass Live Conversion Parameters not found for EcoZone: {} and Species Code: {}. Cannot calculate CFS Biomass",
+					ecoZone, sp64Code
+			);
+			return CfsBiomassVolumeDetails.EMPTY;
+		}
+		double volume = volumeData.closeUtilizationVolume();
+		if (volume == 0.0) {
+			logger.debug("Supplied Volume is zero, so must CFS Biomass values and proportions.");
+			return CfsBiomassVolumeDetails.ZERO_VOLUME;
+		}
+
+		CfsBioProportionToUse propToUse = determineCfsBioProportionToUse(volume, liveParams);
+		double biomassMerchVolume = calculateCfsBiomassMerchVolume(volume, liveParams);
+		double biomassNonMerchVolume = calculateCfsBiomassNonMerchVolume(liveParams, biomassMerchVolume);
+		double totalBio = biomassMerchVolume + biomassNonMerchVolume;
+		double biomassSaplingVolume = calculateCfsBiomassSaplingVolume(liveParams, totalBio);
+		// Commented out because value was not used in the end in VDYP7
+		// double propDead = calculateCfsBiomassPropDead(deadParams, volume);
+
+		double propStemwood;
+		double propBark;
+		double propBranches;
+		double propFoliage;
+		if (propToUse == CfsBioProportionToUse.COMPUTED) {
+			// Compute the proportions of the biomass components
+			double aSum = liveParams.getParm(CfsLiveConversionParams.A1)
+					+ (liveParams.getParm(CfsLiveConversionParams.A2) * volume)
+					+ (liveParams.getParm(CfsLiveConversionParams.A3) * Math.log(5.0 + volume));
+			double bSum = liveParams.getParm(CfsLiveConversionParams.B1)
+					+ (liveParams.getParm(CfsLiveConversionParams.B2) * volume)
+					+ (liveParams.getParm(CfsLiveConversionParams.B3) * Math.log(5.0 + volume));
+			double cSum = liveParams.getParm(CfsLiveConversionParams.C1)
+					+ (liveParams.getParm(CfsLiveConversionParams.C2) * volume)
+					+ (liveParams.getParm(CfsLiveConversionParams.C3) * Math.log(5.0 + volume));
+
+			double aExpr = Math.exp(aSum);
+			double bExpr = Math.exp(bSum);
+			double cExpr = Math.exp(cSum);
+
+			double denom = 1.0 + aExpr + bExpr + cExpr;
+			propStemwood = 1 / denom;
+			propBark = aExpr / denom;
+			propBranches = bExpr / denom;
+			propFoliage = cExpr / denom;
+		} else if (propToUse == CfsBioProportionToUse.LOW_BOUND) {
+			logger.debug("Using Low Bound Proportions for CFS Biomass calculation.");
+			// use minimum values
+			propStemwood = liveParams.getParm(CfsLiveConversionParams.LOW_STEMWOOD_PROP);
+			propBark = liveParams.getParm(CfsLiveConversionParams.LOW_STEMBARK_PROP);
+			propBranches = liveParams.getParm(CfsLiveConversionParams.LOW_BRANCHES_PROP);
+			propFoliage = liveParams.getParm(CfsLiveConversionParams.LOW_FOLIAGE_PROP);
+		} else if (propToUse == CfsBioProportionToUse.HIGH_BOUND) {
+			// use maximum values
+			logger.debug("Using High Bound Proportions for CFS Biomass calculation.");
+			propStemwood = liveParams.getParm(CfsLiveConversionParams.HIGH_STEMWOOD_PROP);
+			propBark = liveParams.getParm(CfsLiveConversionParams.HIGH_STEMBARK_PROP);
+			propBranches = liveParams.getParm(CfsLiveConversionParams.HIGH_BRANCHES_PROP);
+			propFoliage = liveParams.getParm(CfsLiveConversionParams.HIGH_FOLIAGE_PROP);
+		} else {
+			logger.error("Invalid CfsBioProportionToUse: {}. Cannot calculate CFS Biomass proportions.", propToUse);
+			propStemwood = propBark = propBranches = propFoliage = -9.0;
+		}
+
+		double sum = propStemwood + propBark + propBranches + propFoliage;
+		if (propStemwood < 0 || propBark < 0 || propBranches < 0 || propFoliage < 0) {
+			logger.warn(
+					PolygonMessageKind.CFS_BIO_NOT_ALL_PROP_CALCULATED.getTemplate(), propStemwood, propBark,
+					propBranches, propFoliage
+			);
+			polygon.addMessage(
+					new PolygonMessage.Builder().polygon(polygon)
+							.details(
+									ReturnCode.SUCCESS, MessageSeverityCode.WARNING,
+									PolygonMessageKind.CFS_BIO_NOT_ALL_PROP_CALCULATED, propStemwood, propBark,
+									propBranches, propFoliage
+							).build()
+			);
+
+		} else if (Math.abs(1.0 - sum) > 1.0E-5) {
+			logger.error(PolygonMessageKind.CFS_BIO_PROP_DO_NOT_SUM_TO_ONE.getTemplate(), sum);
+			polygon.addMessage(
+					new PolygonMessage.Builder().polygon(polygon)
+							.details(
+									ReturnCode.SUCCESS, MessageSeverityCode.WARNING,
+									PolygonMessageKind.CFS_BIO_PROP_DO_NOT_SUM_TO_ONE, sum
+							).build()
+			);
+		}
+
+		double bioStemwood = -9.0; // default to error code
+		double bioBark = -9.0;
+		double bioBranches = -9.0;
+		double bioFoliage = -9.0;
+		if (biomassMerchVolume >= 0.0) {
+			bioStemwood = biomassMerchVolume + biomassNonMerchVolume + biomassSaplingVolume;
+		}
+		if (bioStemwood >= 0.0 && propStemwood >= 0) {
+			if (propBark > 0.0)
+				bioBark = (bioStemwood / propStemwood) * propBark;
+			if (propBranches > 0.0)
+				bioBranches = (bioStemwood / propStemwood) * propBranches;
+			if (propFoliage > 0.0)
+				bioFoliage = (bioStemwood / propStemwood) * propFoliage;
+		}
+		double bioDead = -9.0; // propDead * (bioStemwood / propStemwood);
+
+		return new CfsBiomassVolumeDetails(
+				biomassMerchVolume, biomassNonMerchVolume, biomassSaplingVolume, propStemwood, propBark, propBranches,
+				propFoliage, bioStemwood, bioBark, bioBranches, bioFoliage, bioDead
+		);
+	}
+
+	/**
+	 * Determines the proportion of CFS Biomass to use based on the close utilization volume and conversion
+	 * coefficients. This influencing factor is if the close Utilization Volume it outside of the minimum or maximum
+	 * thresholds.
+	 * 
+	 * @param volume       Close utilization volume
+	 * @param coefficients The CfsBiomassConversionCoefficientsDetails containing the conversion parameters.
+	 * @return The CfsBioProportionToUse indicating which proportions to use.
+	 */
+	private static CfsBioProportionToUse
+			determineCfsBioProportionToUse(double volume, CfsBiomassConversionCoefficientsDetails coefficients) {
+		if (volume < coefficients.getParm(CfsLiveConversionParams.MIN_VOLUME)) {
+			logger.warn(
+					"CFS Biomass close utilization volume {} is below minimum threshold {}. Use Low Bound Proportions.",
+					volume, coefficients.getParm(CfsLiveConversionParams.MIN_VOLUME)
+			);
+			return CfsBioProportionToUse.LOW_BOUND;
+		}
+		if (volume > coefficients.getParm(CfsLiveConversionParams.MAX_VOLUME)) {
+			logger.warn(
+					"CFS Biomass close utilization volume {} is above maximum threshold {}. Use High Bound Proportions.",
+					volume, coefficients.getParm(CfsLiveConversionParams.MAX_VOLUME)
+			);
+			return CfsBioProportionToUse.HIGH_BOUND;
+		}
+		return CfsBioProportionToUse.COMPUTED;
+	}
+
+	/**
+	 * Calculates the merchantable biomass volume based on the close utilization volume and conversion coefficients.
+	 *
+	 * @param volume       Close utilization volume
+	 * @param coefficients The CfsBiomassConversionCoefficientsDetails containing the conversion parameters.
+	 * @return The calculated merchantable biomass volume.
+	 */
+	private static double
+			calculateCfsBiomassMerchVolume(double volume, CfsBiomassConversionCoefficientsDetails coefficients) {
+
+		if (volume < 0) {
+			logger.error(
+					"CFS Biomass close utilization volume not supplied: {}. Returning -9.0 to indicate error.", volume
+			);
+			return -9.0;
+		}
+		// Compute it
+		return coefficients.getParm(CfsLiveConversionParams.A)
+				* Math.pow(volume, coefficients.getParm(CfsLiveConversionParams.B));
+	}
+
+	/**
+	 * Calculates the non-merchantable biomass volume based on the merchantable biomass volume and conversion
+	 * coefficients.
+	 *
+	 * @param coefficients       The CfsBiomassConversionCoefficientsDetails containing the conversion parameters.
+	 * @param biomassMerchVolume The merchantable biomass volume.
+	 * @return The calculated non-merchantable biomass volume.
+	 */
+	private static double calculateCfsBiomassNonMerchVolume(
+			CfsBiomassConversionCoefficientsDetails coefficients, double biomassMerchVolume
+	) {
+		double min = 1.0;
+		double biomassNonMerchFactor = coefficients.getParm(CfsLiveConversionParams.K_NONMERCH)
+				+ coefficients.getParm(CfsLiveConversionParams.A_NONMERCH)
+						* Math.pow(biomassMerchVolume, coefficients.getParm(CfsLiveConversionParams.B_NONMERCH));
+
+		biomassNonMerchFactor = Math
+				.max(min, Math.min(coefficients.getParm(CfsLiveConversionParams.CAP_NONMERCH), biomassNonMerchFactor));
+		return (biomassNonMerchFactor * biomassMerchVolume) - biomassMerchVolume;
+	}
+
+	/**
+	 * Calculates the sapling biomass volume based on the total biomass and conversion coefficients.
+	 *
+	 * @param coefficients The CfsBiomassConversionCoefficientsDetails containing the conversion parameters.
+	 * @param totalBio     The total biomass volume (merchantable + non-merchantable).
+	 * @return The calculated sapling biomass volume.
+	 */
+	private static double
+			calculateCfsBiomassSaplingVolume(CfsBiomassConversionCoefficientsDetails coefficients, double totalBio) {
+		double min = 1.0;
+		double saplingFactor = coefficients.getParm(CfsLiveConversionParams.K_SAP)
+				+ coefficients.getParm(CfsLiveConversionParams.A_SAP)
+						* Math.pow(totalBio, coefficients.getParm(CfsLiveConversionParams.B_SAP));
+		saplingFactor = Math.max(min, Math.min(coefficients.getParm(CfsLiveConversionParams.CAP_SAP), saplingFactor));
+
+		return (saplingFactor * totalBio) - totalBio;
+	}
+
+	/**
+	 * Calculates the proportion of dead biomass based on the close utilization volume and conversion coefficients.
+	 *
+	 * @param coefficients The CfsBiomassConversionCoefficientsDetails containing the conversion parameters.
+	 * @param volumeData   The EntityVolumeDetails containing the close utilization volume.
+	 * @return The calculated proportion of dead biomass.
+	 */
+	private static double calculateCfsBiomassPropDead(
+			CfsBiomassConversionCoefficientsDetails coefficients, EntityVolumeDetails volumeData
+	) {
+		return -9.0; // simply return error code until we wish to actually use this output
+						// Implementation below
+		/*
+		 * double volume = volumeData.closeUtilizationVolume(); if (volume < 0) { return -9.0; } double
+		 * volIntervalStart; double volIntervalEnd; double propIntervalStart; double propIntervalEnd; if (volume <
+		 * coefficients.getParm(CfsDeadConversionParams.V1)) { volIntervalStart = 0.0; volIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.V1); propIntervalStart =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP1); propIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP2); } else if (volume <
+		 * coefficients.getParm(CfsDeadConversionParams.V2)) { volIntervalStart =
+		 * coefficients.getParm(CfsDeadConversionParams.V1); volIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.V2); propIntervalStart =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP2); propIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP3); } else if (volume <
+		 * coefficients.getParm(CfsDeadConversionParams.V3)) { volIntervalStart =
+		 * coefficients.getParm(CfsDeadConversionParams.V2); volIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.V3); propIntervalStart =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP3); propIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP4); } else if (volume <
+		 * coefficients.getParm(CfsDeadConversionParams.V4)) { volIntervalStart =
+		 * coefficients.getParm(CfsDeadConversionParams.V3); volIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.V4); propIntervalStart =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP4); propIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP5); } else{ volIntervalStart =
+		 * coefficients.getParm(CfsDeadConversionParams.V4); volIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.V4); propIntervalStart =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP5); propIntervalEnd =
+		 * coefficients.getParm(CfsDeadConversionParams.PROP5); }
+		 * 
+		 * if (Math.abs(volIntervalEnd - volIntervalStart) <= 1.0E-5) { // If the volume is exactly equal to the end of
+		 * the interval, return the end of the interval return propIntervalEnd; }
+		 * 
+		 * double volFactor = (volume - volIntervalStart)/(volIntervalEnd - volIntervalStart); return propIntervalStart
+		 * + (volFactor * (propIntervalEnd - propIntervalStart));
+		 * 
+		 */
+
+	}
+
+}
---
+++ lib/vdyp-extended-core/src/test/java/ca/bc/gov/nrs/vdyp/ecore/projection/output/yieldtable/CfsBiomassCalculatorTest.java
@@ -0,0 +1,168 @@
+package ca.bc.gov.nrs.vdyp.ecore.projection.output.yieldtable;
+
+import static ca.bc.gov.nrs.vdyp.test.TestUtils.LAYER_CSV_HEADER_LINE;
+import static ca.bc.gov.nrs.vdyp.test.TestUtils.POLYGON_CSV_HEADER_LINE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import ca.bc.gov.nrs.vdyp.ecore.api.v1.exceptions.AbstractProjectionRequestException;
+import ca.bc.gov.nrs.vdyp.ecore.model.v1.Parameters;
+import ca.bc.gov.nrs.vdyp.ecore.model.v1.ProjectionRequestKind;
+import ca.bc.gov.nrs.vdyp.ecore.projection.ProjectionContext;
+import ca.bc.gov.nrs.vdyp.ecore.projection.input.HcsvPolygonStream;
+import ca.bc.gov.nrs.vdyp.ecore.projection.model.Layer;
+import ca.bc.gov.nrs.vdyp.ecore.projection.model.Polygon;
+import ca.bc.gov.nrs.vdyp.test.TestUtils;
+
+public class CfsBiomassCalculatorTest {
+
+	private final static String TEST_PROJECTION_ID = "TestProjectionId";
+	private final static long TEST_POLYGON_NUMBER = 12345678;
+
+	static Stream<Arguments> biomassAmounts() {
+		return Stream.of(
+				Arguments.of(
+						"6535156131,000A000,56540605,,UNK,UNK,F,UNK,,,,,,,,IDF,,55,1,,V,T,U,TM,OP,,2025,,,,,,,,,,,,,,,,",
+						127.9, 84.56, 9.26, 11.35, 7.21, 0.05
+				), // computed biomass proportions
+				Arguments.of(
+						"6535156131,000A000,56540605,,UNK,UNK,F,UNK,,,,,,,,IDF,,55,1,,V,T,U,TM,OP,,2025,,,,,,,,,,,,,,,,",
+						0.01, 0.054, 0.0079, 0.011, 0.010, 0.001
+				), // low bound biomass proportions
+				Arguments.of(
+						"6535156131,000A000,56540605,,UNK,UNK,F,UNK,,,,,,,,IDF,,55,1,,V,T,U,TM,OP,,2025,,,,,,,,,,,,,,,,",
+						2259.3, 850.09, 65.57, 48.13, 24.98, 0.05
+				), // high bound biomass proportions
+				Arguments.of(
+						"6535156131,000A000,56540605,,UNK,UNK,F,UNK,,,,,,,,IDF,,55,1,,V,T,U,TM,OP,,2025,,,,,,,,,,,,,,,,",
+						0.0, 0.0, 0.0, 0.0, 0.0, 0.00001
+				), // zero biomass proportions,
+				Arguments.of(
+						"6535156131,000A000,56540605,,UNK,UNK,F,UNK,,,,,,,,IDF,,55,1,,V,T,U,TM,OP,,2025,,,,,,,,,,,,,,,,",
+						-9.0, -9.0, -9.0, -9.0, -9.0, 0.00001
+				), // not supplied biomass proportions
+				Arguments.of(
+						"6535156131,000A000,56540605,,UNK,UNK,F,UNK,,,,,,,,IDF,13,55,1,,V,T,U,TM,OP,,2025,,,,,,,,,,,,,,,,",
+						127.9, 84.56, 9.80, 12.55, 8.05, 0.05
+				) // Supplied cfs eco zone
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("biomassAmounts")
+	void testPolygonBiomassCalculation(
+			String polygonData, double closeUtilizationVolume, double stemwood, double bark, double branches,
+			double foliage, double variance
+	) throws AbstractProjectionRequestException, IOException {
+
+		var parameters = new Parameters();
+		parameters.setYearStart(2025);
+		parameters.setYearEnd(2030);
+
+		var context = new ProjectionContext(ProjectionRequestKind.HCSV, TEST_PROJECTION_ID, parameters, false);
+
+		var polygonInputStream = TestUtils.makeInputStream(
+				//
+				POLYGON_CSV_HEADER_LINE, polygonData
+
+		);
+		var layersInputStream = TestUtils.makeInputStream(
+				//
+				LAYER_CSV_HEADER_LINE,
+				"6535156131,6484558358,000A000,56540605,1,P,,1,,PL,16.30,50,,,PL,30.0,AC,30.0,H,30.0,S,10.0,,,,,7,,,,,,,,,,,"
+		);
+
+		var polygonStream = new HcsvPolygonStream(context, polygonInputStream, layersInputStream);
+
+		var polygon = polygonStream.getNextPolygon();
+
+		var entityVolumeDetails = new EntityVolumeDetails(
+				closeUtilizationVolume, closeUtilizationVolume, closeUtilizationVolume, closeUtilizationVolume,
+				closeUtilizationVolume
+		);
+
+		var cfsBiomass = CfsBiomassCalculator.calculateBiomassPolygonVolumeDetails(entityVolumeDetails, polygon);
+		var totalProp = cfsBiomass.propStemwood() + cfsBiomass.propBark() + cfsBiomass.propBranches()
+				+ cfsBiomass.propFoliage();
+		if (closeUtilizationVolume != 0.0) {
+			assertThat(totalProp, closeTo(1.0, 0.001));
+		}
+		assertThat(cfsBiomass.bioStemwood(), closeTo(stemwood, variance));
+		assertThat(cfsBiomass.bioBark(), closeTo(bark, variance));
+		assertThat(cfsBiomass.bioBranches(), closeTo(branches, variance));
+		assertThat(cfsBiomass.bioFoliage(), closeTo(foliage, variance));
+
+	}
+
+	@Test
+	void testLayerCalculation() throws AbstractProjectionRequestException {
+		var parameters = new Parameters();
+		parameters.setYearStart(2025);
+		parameters.setYearEnd(2030);
+
+		var context = new ProjectionContext(ProjectionRequestKind.HCSV, TEST_PROJECTION_ID, parameters, false);
+
+		var polygonInputStream = TestUtils.makeInputStream(
+				//
+				POLYGON_CSV_HEADER_LINE,
+				"6535156131,000A000,56540605,,UNK,UNK,F,UNK,,,,,,,,IDF,,55,1,,V,T,U,TM,OP,,2025,,,,,,,,,,,,,,,,"
+		);
+		var layersInputStream = TestUtils.makeInputStream(
+				//
+				LAYER_CSV_HEADER_LINE,
+				"6535156131,6484558358,000A000,56540605,1,P,,1,,FD,16.30,50,,,FD,30.0,AC,30.0,H,30.0,S,10.0,,,,,7,,,,,,,,,,,",
+				"6535156131,6484558358,000A000,56540605,2,V,,1,,PL,16.30,50,,,PL,30.0,AC,30.0,H,30.0,S,10.0,,,,,7,,,,,,,,,,,"
+		);
+
+		var polygonStream = new HcsvPolygonStream(context, polygonInputStream, layersInputStream);
+
+		var polygon = polygonStream.getNextPolygon();
+		double closeUtilizationVolume = 127.9;
+		double stemwood = 84.56;
+		double bark = 9.26;
+		double branches = 11.35;
+		double foliage = 7.21;
+		double variance = 0.05;
+		var entityVolumeDetails = new EntityVolumeDetails(
+				closeUtilizationVolume, closeUtilizationVolume, closeUtilizationVolume, closeUtilizationVolume,
+				closeUtilizationVolume
+		);
+
+		var cfsBiomass = CfsBiomassCalculator
+				.calculateBiomassLayerVolumeDetails(entityVolumeDetails, polygon, polygon.getVeteranLayer());
+		var totalProp = cfsBiomass.propStemwood() + cfsBiomass.propBark() + cfsBiomass.propBranches()
+				+ cfsBiomass.propFoliage();
+		assertThat(totalProp, closeTo(1.0, 0.001));
+		assertThat(cfsBiomass.bioStemwood(), closeTo(stemwood, variance));
+		assertThat(cfsBiomass.bioBark(), closeTo(bark, variance));
+		assertThat(cfsBiomass.bioBranches(), closeTo(branches, variance));
+		assertThat(cfsBiomass.bioFoliage(), closeTo(foliage, variance));
+	}
+
+	@Test
+	void testDeadLayerNoCalculation() {
+		var polygon = new Polygon.Builder().polygonNumber(TEST_POLYGON_NUMBER).build();
+		var layer = new Layer.Builder().layerId("3").polygon(polygon).isDeadLayer(true).build();
+		polygon.assignDeadLayer(layer, 2025, 50.0);
+		var entityVolumeDetails = new EntityVolumeDetails(100.0, 98.0, 90.0, 80.0, 70.0);
+		var cfsBiomass = CfsBiomassCalculator.calculateBiomassLayerVolumeDetails(entityVolumeDetails, polygon, layer);
+		assertThat(cfsBiomass.cfsBiomassMerch(), is(-9.0));
+	}
+
+	@Test
+	void testNullPolygon() {
+		var entityVolumeDetails = new EntityVolumeDetails(100.0, 98.0, 90.0, 80.0, 70.0);
+		var cfsBiomass = CfsBiomassCalculator.calculateBiomassPolygonVolumeDetails(entityVolumeDetails, null);
+		assertThat(cfsBiomass.cfsBiomassMerch(), is(-9.0));
+	}
+
+}
