2342646093c3ee8ac91e8e2aade2eeb6ea71c604
Peter Minter <pminter@vividsolutions.com>
2025-12-04 12:29:16 -0800

    Add explanation for Sonar cube code smell

--- backend/src/main/java/ca/bc/gov/nrs/vdyp/backend/data/models/UserTypeCodeModel.java
+++ backend/src/main/java/ca/bc/gov/nrs/vdyp/backend/data/models/UserTypeCodeModel.java
@@ -1,5 +1,10 @@
 package ca.bc.gov.nrs.vdyp.backend.data.models;
 
+/**
+ * Model layer object to represent a Projection Status. Equality and hashCode are filly defined in the parent class
+ * {@link CodeTableModel}. (explicitly calling the abstract getCode() method for the sake of comparison
+ */
+@SuppressWarnings("squid:S2160")
 public class UserTypeCodeModel extends CodeTableModel {
 	private String userTypeCode;
 
