08d4f6a1cf9448d1f5dfbb866f64bd76d96b92b1
Peter Minter <pminter@vividsolutions.com>
2025-12-10 08:55:02 -0800

    Add find by owner to Repository for list page

--- backend/src/main/java/ca/bc/gov/nrs/vdyp/backend/data/repositories/ProjectionRepository.java
+++ backend/src/main/java/ca/bc/gov/nrs/vdyp/backend/data/repositories/ProjectionRepository.java
@@ -1,10 +1,16 @@
 package ca.bc.gov.nrs.vdyp.backend.data.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import ca.bc.gov.nrs.vdyp.backend.data.entities.ProjectionEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class ProjectionRepository implements PanacheRepositoryBase<ProjectionEntity, UUID> {
+	public List<ProjectionEntity> findByOwner(UUID vdypUserGUID) {
+		return list("ownerUser.vdypUserGUID", vdypUserGUID);
+	}
 
 }
