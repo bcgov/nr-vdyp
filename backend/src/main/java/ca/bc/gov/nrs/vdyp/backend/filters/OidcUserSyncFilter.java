bf7dcd3b68aab18dfd4df7e7ef1802b39b5410ad
Peter Minter <pminter@vividsolutions.com>
2025-12-10 08:53:19 -0800

    Surface authenticated  VDYP User to the Request

--- backend/src/main/java/ca/bc/gov/nrs/vdyp/backend/filters/OidcUserSyncFilter.java
+++ backend/src/main/java/ca/bc/gov/nrs/vdyp/backend/filters/OidcUserSyncFilter.java
@@ -2,10 +2,12 @@ package ca.bc.gov.nrs.vdyp.backend.filters;
 
 import org.jboss.resteasy.reactive.server.ServerRequestFilter;
 
+import ca.bc.gov.nrs.vdyp.backend.context.CurrentVDYPUser;
 import ca.bc.gov.nrs.vdyp.backend.services.VDYPUserService;
 import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.ext.Provider;
@@ -19,6 +21,9 @@ public class OidcUserSyncFilter {
 
 	VDYPUserService userService;
 
+	@Inject
+	CurrentVDYPUser currentUser;
+
 	public OidcUserSyncFilter(SecurityIdentity identity, VDYPUserService userService) {
 		this.identity = identity;
 		this.userService = userService;
@@ -31,6 +36,8 @@ public class OidcUserSyncFilter {
 		}
 
 		// This will get or create the VDYPUser for this token
-		userService.ensureVDYPUserFromSecurityIdentity(identity);
+		var vdypUser = userService.ensureVDYPUserFromSecurityIdentity(identity);
+		// set it for the request context
+		currentUser.setUser(vdypUser);
 	}
 }
