030f3e8af3bc582fa1e6e36236a4f5504eac922f
Peter Minter <pminter@vividsolutions.com>
2026-01-12 13:24:46 -0800

    Remove unused s3 access headers

--- batch/src/main/java/ca/bc/gov/nrs/vdyp/batch/client/coms/ComsRestClientConfig.java
+++ batch/src/main/java/ca/bc/gov/nrs/vdyp/batch/client/coms/ComsRestClientConfig.java
@@ -12,14 +12,6 @@ public class ComsRestClientConfig {
 		return RestClient.builder().baseUrl(props.baseUrl()).defaultHeaders(h -> {
 			h.setBasicAuth(props.basicAuth().username(), props.basicAuth().password());
 
-			if (props.s3access() != null) {
-				if (props.s3access().bucket() != null && !props.s3access().bucket().isBlank()) {
-					h.add("x-amz-bucket", props.s3access().bucket());
-				}
-				if (props.s3access().endpoint() != null && !props.s3access().endpoint().isBlank()) {
-					h.add("x-amz-endpoint", props.s3access().endpoint());
-				}
-			}
 		}).build();
 	}
 }
