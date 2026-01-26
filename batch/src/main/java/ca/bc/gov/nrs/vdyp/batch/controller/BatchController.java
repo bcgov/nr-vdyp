b540070d22030af9b3eacbec3ded07c562d8a6b1
Peter Minter <pminter@vividsolutions.com>
2026-01-26 10:44:16 -0800

    Formatting

--- batch/src/main/java/ca/bc/gov/nrs/vdyp/batch/controller/BatchController.java
+++ batch/src/main/java/ca/bc/gov/nrs/vdyp/batch/controller/BatchController.java
@@ -122,8 +122,7 @@ public class BatchController {
 	/**
 	 * Start a new batch job execution
 	 */
-	@PostMapping(
-			value = "/startWithGUIDs", //
+	@PostMapping(value = "/startWithGUIDs", //
 			consumes = MediaType.MULTIPART_FORM_DATA_VALUE, //
 			produces = MediaType.APPLICATION_JSON_VALUE
 
