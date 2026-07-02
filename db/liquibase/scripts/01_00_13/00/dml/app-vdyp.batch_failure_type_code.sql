bb37d4d0606010209cb59a57fed68b965710b3fb
Peter Minter <pminter@vividsolutions.com>
2026-07-02 14:04:01 -0700

    Update Liquibase to create a type table and grants to support complete failure type system

---
+++ db/liquibase/scripts/01_00_13/00/dml/app-vdyp.batch_failure_type_code.sql
@@ -0,0 +1,4 @@
+INSERT INTO "app-vdyp".batch_failure_type_code (batch_failure_type_code,description,display_order,effective_date,expiry_date,revision_count,create_user,create_date,update_user,update_date) VALUES
+	 ('INPUT','Processing Inputs',1,'2026-07-02','9999-12-31',1,'System','2026-07-02','System','2026-07-02'),
+	 ('PROCESS','Projecting Stands',2,'2026-07-02','9999-12-31',1,'System','2026-07-02','System','2026-07-02'),
+	 ('OUTPUT','Aggregating Results',3,'2026-07-02','9999-12-31',1,'System','2026-07-02','System','2026-07-02');
---
+++ db/liquibase/scripts/01_00_13/01/grants/app-vdyp.ddl.apply_grants.sql
@@ -0,0 +1,1 @@
+GRANT SELECT, INSERT, UPDATE, DELETE ON "app-vdyp".batch_failure_type_code TO "app_vdyp_rest_proxy";
