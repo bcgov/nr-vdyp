1f50818a14619903b500d05f3453544a00540aff
Kevin Smith <ksmith@vividsolutions.com>
2026-07-14 17:23:17 -0700

    More unit tests for RealComonentRunner

--- lib/vdyp-extended-core/src/test/java/ca/bc/gov/nrs/vdyp/ecore/projection/RealComponentRunnerTest.java
+++ lib/vdyp-extended-core/src/test/java/ca/bc/gov/nrs/vdyp/ecore/projection/RealComponentRunnerTest.java
@@ -2,18 +2,30 @@ package ca.bc.gov.nrs.vdyp.ecore.projection;
 
 import static ca.bc.gov.nrs.vdyp.test.TestUtils.LAYER_CSV_HEADER_LINE;
 import static ca.bc.gov.nrs.vdyp.test.TestUtils.POLYGON_CSV_HEADER_LINE;
+import static ca.bc.gov.nrs.vdyp.test.VdypMatchers.causedBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import ca.bc.gov.nrs.vdyp.application.VdypApplication;
+import ca.bc.gov.nrs.vdyp.application.VdypApplicationIdentifier;
+import ca.bc.gov.nrs.vdyp.common.VdypApplicationInitializationException;
+import ca.bc.gov.nrs.vdyp.common.VdypApplicationProcessingException;
 import ca.bc.gov.nrs.vdyp.ecore.api.v1.exceptions.PolygonExecutionException;
 import ca.bc.gov.nrs.vdyp.ecore.api.v1.exceptions.YieldTableGenerationException;
 import ca.bc.gov.nrs.vdyp.ecore.model.v1.Parameters;
@@ -153,5 +165,223 @@ class RealComponentRunnerTest {
 		long reportTitleCount = content.lines().filter(line -> line.contains("VDYP Yield Table Report")).count();
 		assertThat(reportTitleCount, is(1L));
 		assertThat(content, containsString("CFS Biomass"));
+	}
+
+	@Nested
+	class RunApp {
+
+		@Test
+		void testSuccess() throws Exception {
+			var em = EasyMock.createControl();
+			var runner = new RealComponentRunner();
+
+			Polygon polygon = em.createMock(Polygon.class);
+			ProjectionTypeCode projectionTypeCode = ProjectionTypeCode.PRIMARY;
+			PolygonProjectionState state = em.createMock(PolygonProjectionState.class);
+			VdypApplicationIdentifier appToUse = VdypApplicationIdentifier.FIP_START;
+			List<String> controlFiles = List.of("Test.CTR");
+			VdypApplication<?> app = em.createMock(VdypApplication.class);
+
+			Supplier<VdypApplication<?>> getApp = () -> app;
+			Consumer<VdypApplication<?>> after = em.createMock(Consumer.class);
+
+			EasyMock.expect(state.getExecutionFolder()).andStubReturn(Path.of("TestPath"));
+
+			// Run the main processing
+			app.doMain(EasyMock.eq(Path.of("TestPath", "PRIMARY", "Test.CTR")));
+			EasyMock.expectLastCall().once(); // Success
+
+			// Handle success
+			state.setProcessingResults(ProjectionStageCode.Initial, projectionTypeCode, Optional.empty());
+			EasyMock.expectLastCall().once();
+
+			//Do any postprocessing
+			after.accept(app);
+			EasyMock.expectLastCall().once();
+
+			// it should clean up after itself
+			app.close();
+			EasyMock.expectLastCall().once();
+
+			em.replay();
+
+			runner.runApp(polygon, projectionTypeCode, state, appToUse, controlFiles, getApp, after);
+
+			em.verify();
+		}
+
+		@Test
+		void testInitializationException() throws Exception {
+			var em = EasyMock.createControl();
+			var runner = new RealComponentRunner();
+
+			Polygon polygon = em.createMock(Polygon.class);
+			ProjectionTypeCode projectionTypeCode = ProjectionTypeCode.PRIMARY;
+			PolygonProjectionState state = em.createMock(PolygonProjectionState.class);
+			VdypApplicationIdentifier appToUse = VdypApplicationIdentifier.FIP_START;
+			List<String> controlFiles = List.of("Test.CTR");
+			VdypApplication<?> app = em.createMock(VdypApplication.class);
+
+			var rootCause = new RuntimeException("Test Exception");
+			var appCause = new VdypApplicationInitializationException(rootCause);
+
+			Supplier<VdypApplication<?>> getApp = () -> app;
+			Consumer<VdypApplication<?>> after = em.createMock(Consumer.class);
+
+			EasyMock.expect(state.getExecutionFolder()).andStubReturn(Path.of("TestPath"));
+
+			// Run the main processing
+			app.doMain(EasyMock.eq(Path.of("TestPath", "PRIMARY", "Test.CTR")));
+			EasyMock.expectLastCall().andThrow(appCause).once();
+
+			// Handle Failure
+			state.setProcessingResults(ProjectionStageCode.Initial, projectionTypeCode, Optional.of(rootCause));
+			EasyMock.expectLastCall().once();
+
+			// it should clean up after itself
+			app.close();
+			EasyMock.expectLastCall().once();
+
+			em.replay();
+
+			runner.runApp(polygon, projectionTypeCode, state, appToUse, controlFiles, getApp, after);
+
+			em.verify();
+		}
+
+		@Test
+		void testProcessingException() throws Exception {
+			var em = EasyMock.createControl();
+			var runner = new RealComponentRunner();
+
+			Polygon polygon = em.createMock(Polygon.class);
+			ProjectionTypeCode projectionTypeCode = ProjectionTypeCode.PRIMARY;
+			PolygonProjectionState state = em.createMock(PolygonProjectionState.class);
+			VdypApplicationIdentifier appToUse = VdypApplicationIdentifier.FIP_START;
+			List<String> controlFiles = List.of("Test.CTR");
+			VdypApplication<?> app = em.createMock(VdypApplication.class);
+
+			var rootCause = new RuntimeException("Test Exception");
+			var appCause = new VdypApplicationProcessingException(rootCause);
+
+			Supplier<VdypApplication<?>> getApp = () -> app;
+			Consumer<VdypApplication<?>> after = em.createMock(Consumer.class);
+
+			EasyMock.expect(state.getExecutionFolder()).andStubReturn(Path.of("TestPath"));
+
+			// Run the main processing
+			app.doMain(EasyMock.eq(Path.of("TestPath", "PRIMARY", "Test.CTR")));
+			EasyMock.expectLastCall().andThrow(appCause).once();
+
+			// Handle Failure
+			state.setProcessingResults(ProjectionStageCode.Initial, projectionTypeCode, Optional.of(rootCause));
+			EasyMock.expectLastCall().once();
+
+			// it should clean up after itself
+			app.close();
+			EasyMock.expectLastCall().once();
+
+			em.replay();
+
+			runner.runApp(polygon, projectionTypeCode, state, appToUse, controlFiles, getApp, after);
+
+			em.verify();
+		}
+
+		@Test
+		void testRuntimeException() throws Exception {
+			var em = EasyMock.createControl();
+			var runner = new RealComponentRunner();
+
+			Polygon polygon = em.createMock(Polygon.class);
+			ProjectionTypeCode projectionTypeCode = ProjectionTypeCode.PRIMARY;
+			PolygonProjectionState state = em.createMock(PolygonProjectionState.class);
+			VdypApplicationIdentifier appToUse = VdypApplicationIdentifier.FIP_START;
+			List<String> controlFiles = List.of("Test.CTR");
+			VdypApplication<?> app = em.createMock(VdypApplication.class);
+
+			var rootCause = new RuntimeException("Test Exception");
+
+			Supplier<VdypApplication<?>> getApp = () -> app;
+			Consumer<VdypApplication<?>> after = em.createMock(Consumer.class);
+
+			EasyMock.expect(state.getExecutionFolder()).andStubReturn(Path.of("TestPath"));
+
+			// Run the main processing
+			app.doMain(EasyMock.eq(Path.of("TestPath", "PRIMARY", "Test.CTR")));
+			EasyMock.expectLastCall().andThrow(rootCause).once();
+
+			// Store exception result
+			state.setProcessingResults(ProjectionStageCode.Initial, projectionTypeCode, Optional.of(rootCause));
+			EasyMock.expectLastCall().once();
+
+			// it should clean up after itself
+			app.close();
+			EasyMock.expectLastCall().once();
+
+			em.replay();
+
+			var ex = assertThrows(
+					PolygonExecutionException.class, () -> runner.runApp(
+							polygon, projectionTypeCode, state, appToUse, controlFiles, getApp, after
+					)
+			);
+
+			assertThat(ex, causedBy(sameInstance(rootCause)));
+
+			em.verify();
+		}
+
+		@Test
+		void testExceptionWhileClosing() throws Exception {
+			var em = EasyMock.createControl();
+			var runner = new RealComponentRunner();
+
+			Polygon polygon = em.createMock(Polygon.class);
+			ProjectionTypeCode projectionTypeCode = ProjectionTypeCode.PRIMARY;
+			PolygonProjectionState state = em.createMock(PolygonProjectionState.class);
+			VdypApplicationIdentifier appToUse = VdypApplicationIdentifier.FIP_START;
+			List<String> controlFiles = List.of("Test.CTR");
+			VdypApplication<?> app = em.createMock(VdypApplication.class);
+
+			var rootCause = new RuntimeException("Test Exception");
+
+			Supplier<VdypApplication<?>> getApp = () -> app;
+			Consumer<VdypApplication<?>> after = em.createMock(Consumer.class);
+
+			EasyMock.expect(state.getExecutionFolder()).andStubReturn(Path.of("TestPath"));
+
+			// Run the main processing
+			app.doMain(EasyMock.eq(Path.of("TestPath", "PRIMARY", "Test.CTR")));
+			EasyMock.expectLastCall().once();
+
+			// Handle Success
+			state.setProcessingResults(ProjectionStageCode.Initial, projectionTypeCode, Optional.empty());
+			EasyMock.expectLastCall().once();
+
+			//Do any postprocessing
+			after.accept(app);
+			EasyMock.expectLastCall().once();
+
+			// Error while cleaning up
+			app.close();
+			EasyMock.expectLastCall().andThrow(rootCause).once();
+
+			// Store exception result
+			state.setProcessingResults(ProjectionStageCode.of(appToUse), projectionTypeCode, Optional.of(rootCause));
+			EasyMock.expectLastCall().once();
+
+			em.replay();
+
+			var ex = assertThrows(
+					PolygonExecutionException.class, () -> runner.runApp(
+							polygon, projectionTypeCode, state, appToUse, controlFiles, getApp, after
+					)
+			);
+
+			assertThat(ex, causedBy(sameInstance(rootCause)));
+
+			em.verify();
+		}
 	}
 }
