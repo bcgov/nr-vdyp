536b0f82ecdb2bd90c2e6d38d7af0e09d7a11a78
Roy Jeong <roy.jeong@gov.bc.ca>
2026-08-12 11:32:28 -0700

    Enable re-run for Failed projections, matching Draft/Admin-Cancelled permissions

--- frontend/src/views/ProjectionDetail.vue
+++ frontend/src/views/ProjectionDetail.vue
@@ -248,7 +248,7 @@
         <ReportSettingsPanel ref="reportSettingsPanelRef" class="panel-spacing" />
         <RunProjectionButtonPanel
           v-if="!appStore.isReadOnly || isRunning || isStuck"
-          :isDisabled="!modelParameterStore.runModelEnabled || !isDraftOrAdminCancelled"
+          :isDisabled="!modelParameterStore.runModelEnabled || !isEditableProjectionStatus"
           :showCancelButton="isRunning || isStuck || isQueued"
           :showRevertCancelButton="!(isRunning||isStuck||isQueued)"
           :isRevertCancelDisabled="isCancelDisabled"
@@ -295,8 +295,8 @@
         <MinimumDBHPanel class="panel-spacing" />
         <AttachmentsPanel class="panel-spacing" />
         <RunProjectionButtonPanel
-          v-if="!isRunProgressBarVisible || isAdminCancelled"
-          :isDisabled="!fileUploadStore.runModelEnabled || !isDraftOrAdminCancelled"
+          v-if="isEditableProjectionStatus"
+          :isDisabled="!fileUploadStore.runModelEnabled || !isEditableProjectionStatus"
           :showCancelButton="false"
           :disabledText="fileUploadDisabledText"
           cardActionsClass="card-actions"
@@ -399,8 +399,9 @@ const isFailed = computed(() => appStore.currentProjectionStatus === CONSTANTS.P
 const isCancelled = computed(() => appStore.currentProjectionStatus === CONSTANTS.PROJECTION_STATUS.CANCELLED)
 const isAdminCancelled = computed(() => appStore.currentProjectionStatus === CONSTANTS.PROJECTION_STATUS.ADMN_CNCLD)
 const isDownloadReady = computed(() => appStore.currentProjectionStatus === CONSTANTS.PROJECTION_STATUS.READY)
-// ADMN_CNCLD behaves like Draft
-const isDraftOrAdminCancelled = computed(() => isDraft.value || isAdminCancelled.value)
+// Draft, Failed, and Admin Cancelled all share the same backend permitted-action set (UPDATE/DELETE/READ)
+// and can be edited and re-run
+const isEditableProjectionStatus = computed(() => isDraft.value || isFailed.value || isAdminCancelled.value)
 
 // Shown for File Upload mode when projection has been run (Running/Ready/Failed/Cancelled/Admin Cancelled)
 const isRunProgressBarVisible = computed(
@@ -647,7 +648,7 @@ const fileUploadCompletedCount = computed(() => {
 })
 
 const fileUploadDisabledText = computed(() => {
-  if (!isDraftOrAdminCancelled.value) {
+  if (!isEditableProjectionStatus.value) {
     return `This projection may not be run with a status of ${appStore.currentProjectionStatus}`
   }
   if (!fileUploadStore.runModelEnabled) {
