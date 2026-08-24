9bb7918a1330b25f9a10d0ed24bbe5799b8e600f
Peter Minter <pminter@vividsolutions.com>
2026-08-24 10:56:18 -0700

    Update column name to lease_token_guid for consistency

--- db/liquibase/scripts/01_00_18/00/ddl/tables/batch-job-claim.sql
+++ db/liquibase/scripts/01_00_18/00/ddl/tables/batch-job-claim.sql
@@ -2,15 +2,15 @@
 CREATE TABLE "batch"."batch_job_claim" (
 	projection_guid UUID PRIMARY KEY,     -- Projection GUID is the identifier of the projection being processed.
 	owner_id VARCHAR(512) NOT NULL,       -- Owner ID is a unique string to the replica that secures claims
-	lease_token UUID NOT NULL,            -- Lease Token is a randomly generated UUID known by the owner to secure updates
+    lease_token_guid UUID NOT NULL,       -- Lease Token GUID is a randomly generated UUID known by the owner to secure updates
 	acquired_time TIMESTAMP NOT NULL DEFAULT clock_timestamp(), -- Acquired Time is the time the claim was acquired
 	lease_expiry_time TIMESTAMP NOT NULL, -- Lease expired time is the time the lease will expire if no updates occur
 	version BIGINT NOT NULL DEFAULT 0     -- Version is incremented each time the record is updated, ensuring atomic updates
 );
 
 /* Create Indexes */
-CREATE INDEX "batch"."batch_job_claim_lease_expiry_idx"
-	ON batch_job_claim (lease_expiry_time);
+CREATE INDEX "batch_job_claim_lease_expiry_idx"
+    ON "batch"."batch_job_claim" (lease_expiry_time);
 
 /* Create Table Comments, Sequences for Autonumber Columns */
 
@@ -26,8 +26,9 @@ COMMENT ON COLUMN "batch"."batch_job_claim"."owner_id"
 	IS 'Owner ID is a unique identifier known by the replica that owns the claim to the Job.'
 ;
 
-COMMENT ON COLUMN "batch"."batch_job_claim"."lease_token"
-	IS 'Lease Token is a randomly generated UUID to secure updates to the claim.'
+COMMENT
+ON COLUMN "batch"."batch_job_claim"."lease_token_guid"
+	IS 'Lease Token GUID is a randomly generated UUID to secure updates to the claim.'
 ;
 
 COMMENT ON COLUMN "batch"."batch_job_claim"."acquired_time"
