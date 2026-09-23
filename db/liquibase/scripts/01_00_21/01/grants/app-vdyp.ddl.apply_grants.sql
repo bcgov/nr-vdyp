2959187e52cc5a2fd5ed0110a6542794c93dc5c1
Roy Jeong <roy.jeong@gov.bc.ca>
2026-09-23 13:45:50 -0700

    VDYP-1277: Grant app_vdyp_rest_proxy access to storage cleanup tables

---
+++ db/liquibase/scripts/01_00_21/01/grants/app-vdyp.ddl.apply_grants.sql
@@ -0,0 +1,2 @@
+GRANT SELECT, INSERT, UPDATE, DELETE ON "app-vdyp".storage_cleanup_run TO "app_vdyp_rest_proxy";
+GRANT SELECT, INSERT, UPDATE, DELETE ON "app-vdyp".storage_cleanup_deleted_folder TO "app_vdyp_rest_proxy";
