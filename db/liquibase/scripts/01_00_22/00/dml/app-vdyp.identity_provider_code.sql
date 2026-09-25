8605b7ac2ad78717f319b0e28932c061d6097ec0
Peter Minter <pminter@vividsolutions.com>
2026-09-25 07:21:47 -0700

    Add BC Services card identity provider code

---
+++ db/liquibase/scripts/01_00_22/00/dml/app-vdyp.identity_provider_code.sql
@@ -0,0 +1,2 @@
+INSERT INTO "app-vdyp".identity_provider_code (identity_provider_code,description,display_order,effective_date,expiry_date,revision_count,create_user,create_date,update_user,update_date) VALUES
+	 ('BCSC','BC Services Card',1,'2026-09-24','9999-12-31',1,'System','2026-09-24','System','2026-09-24')
